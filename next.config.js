175b683cdff795eeb753459f7891e6602b2c0cfd
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-09-23 15:33:45 +0800

    feat: Add remote image support for Unsplash in Next.js configuration
    - Introduced remotePatterns in the Next.js configuration to allow images from Unsplash.
    - This enhancement enables the application to fetch and display images hosted on the specified remote server, improving content flexibility.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
